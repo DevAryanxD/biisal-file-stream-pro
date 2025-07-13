6eb82dc0c0f1e1526fd1818f757dc2e3f27bb044
DevAryanxD <144301572+DevAryanxD@users.noreply.github.com>
2025-07-13 21:36:43 +0530

    Update __main__.py

--- biisal/__main__.py
+++ biisal/__main__.py
@@ -15,6 +15,10 @@ from .server import web_server
 from .utils.keepalive import ping_server
 from biisal.bot.clients import initialize_clients
 
+import pyrogram.utils
+
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+
 LOGO = """
  ____ ___ ___ ____    _    _     
 | __ )_ _|_ _/ ___|  / \  | |    
